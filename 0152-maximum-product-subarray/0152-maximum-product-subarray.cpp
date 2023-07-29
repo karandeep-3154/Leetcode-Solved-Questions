2246b98f6c0fbc7a9354d609b9fa6529a170f7de
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-07-29 14:49:54 +0530

    Time: 3 ms (97.14%), Space: 13.9 MB (16.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-//refer https://www.youtube.com/watch?v=tHNsZHXnYd4
     int maxProduct(vector<int>& nums) {
         
-        int maxi = nums[0], mini=nums[0], ans=nums[0];
-        cout<<maxi<<"   "<<mini<<endl;
+        int maxi = nums[0], mini = nums[0], ans = nums[0];
+
         for(int i=1;i<nums.size();i++){
 
-            if(nums[i] < 0){
-                int temp = maxi;
-                maxi = max(nums[i], mini*nums[i]);
-                mini = min(nums[i], temp*nums[i]);
-            }
+            if(nums[i]<0)
+            swap(maxi, mini);
 
-            else{
-                maxi = max(nums[i], maxi*nums[i]);
-                mini = min(nums[i], mini*nums[i]);
-            }
-        //cout<<"i " << i<<"    "<<maxi<<"   "<<mini<<endl;
+            maxi = max(nums[i], nums[i]*maxi);
+            mini = min(nums[i], nums[i]*mini);
 
-//note: we could also just write maxi = max(nums[i], maxi*nums[i]); mini = min(nums[i], mini*nums[i]); for every i and just swap maxi and mini if nums[i]<0
-
-            ans =  max(ans,maxi);
+            ans = max(ans, maxi);
 
         }
+
         return ans;
+
     }
 };
