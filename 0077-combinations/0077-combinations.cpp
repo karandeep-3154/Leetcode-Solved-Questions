84c32017f1c27736f0e9cbbcc19bdbeb47b613e5
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-01 11:04:45 +0530

    Time: 39 ms (31.18%), Space: 51 MB (19.73%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    void backtrack(int n, int k, int start, vector<int>& cur, vector<vector<int>>& ans) {
-        if (k == 0) {
+
+    void solve(int n, int k, vector<int> cur, vector<vector<int>> &ans){
+
+        if(k == 0){
+
             ans.push_back(cur);
             return;
+
         }
 
-        for (int i = start; i <= n; i++) {
-            cur.push_back(i);
-            backtrack(n, k - 1, i + 1, cur, ans);
-            cur.pop_back();
-        }
+        if(k>n)
+        return;
+
+        if(n == 0)
+        return;
+
+        solve(n-1, k, cur, ans);
+        cur.push_back(n);
+        solve(n-1, k-1, cur, ans);
+
     }
 
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
+        
         vector<int> cur;
-        backtrack(n, k, 1, cur, ans);
+        vector<vector<int>> ans;
+
+        solve(n, k, cur, ans);
+
         return ans;
+
     }
 };
