cb72feadb53b160816b5f230e11ec81fb1309b1d
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-19 00:20:43 +0530

    Time: 66 ms (46.13%), Space: 9.1 MB (68.24%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int dp[21][2001]; // Since the maximum possible sum of nums can be 1000, the range of values for j is -1000 to 1000.
+        int n = nums.size();
+        
+        memset(dp, 0, sizeof(dp));
+        dp[0][1000] = 1; // To handle negative indices, shift by 1000
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= 2000; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+                if (j + nums[i - 1] <= 2000) {
+                    dp[i][j] += dp[i - 1][j + nums[i - 1]];
+                }
+            }
+        }
+        
+        return dp[n][target + 1000]; // Return the count for the target sum
+    }
+};
