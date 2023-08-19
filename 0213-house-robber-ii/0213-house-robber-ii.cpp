7c6eaf3dac551cac53ffffb50e1526c2271728d4
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-19 17:02:30 +0530

    Time: 4 ms (28.43%), Space: 7.7 MB (78.22%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,46 +1,58 @@
 class Solution {
 public:
 
-    int solve(vector<int>& nums, int i, bool takelast){
-
-        if(i >= nums.size() or (i == nums.size()-1 and takelast == false))
-        return 0;
-
-        return max(solve(nums,i+1,takelast), solve(nums,i+2,takelast) + nums[i]);
-
-    }
-
-    int solvedp(vector<int>& nums){
+    int calculate(vector<int>& nums, int takefirst) {
+        
+        int dp[101][2];
+        memset(dp, 0, sizeof(dp));
 
         int n = nums.size();
 
-        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        if(takefirst == 1){//take first element skip last element
 
-        for(int i=n-1;i>=0;i--){
+            dp[0][0] = 0;//not take 0th index element
+            dp[0][1] = nums[0];//take 0th index element
 
-            for(int takelast=0;takelast<2;takelast++){
+            for(int i=1;i<n-1;i++){
 
-                if(i == n-1 and takelast == 0)
-                continue;
-
-                dp[i][takelast] = max(dp[i+1][takelast], nums[i] + dp[i+2][takelast]);
-
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+                dp[i][1] = dp[i-1][0] + nums[i];
+                
             }
 
+            return max(dp[n-2][0], dp[n-2][1]);
+
         }
-        return max(dp[0][0], dp[1][1]);
+
+        else{//skip first element and take last element of Array
+
+            dp[1][0] = 0;//not take 1th index element
+            dp[1][1] = nums[1];//take 1th index element
+
+            for(int i=2;i<n;i++){
+
+                dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+                dp[i][1] = dp[i-1][0] + nums[i];
+                
+            }
+
+            return max(dp[n-1][0], dp[n-1][1]);
+
+        }
+
+        return -1;
+
     }
 
     int rob(vector<int>& nums) {
 
         if(nums.size() == 0)
         return 0;
-        
-        if(nums.size() == 1)
+
+        else if(nums.size() == 1)
         return nums[0];
         
-        // return max(solve(nums,0,false), solve(nums,1,true));
-        
-        return solvedp(nums);
+        return max(calculate(nums,0), calculate(nums,1));
+
     }
 };
