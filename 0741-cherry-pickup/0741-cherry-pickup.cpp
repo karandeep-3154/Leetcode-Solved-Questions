768e79c2f16688743bd5867be227e1d3d998a437
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-22 22:11:29 +0530

    Time: 35 ms (90.87%), Space: 9.6 MB (82.51%) - LeetHub

---
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+// //(r1==n-1 && c1==n-1) and (r2==n-1 && c2==n-1) will occur at the same time because r1+c1=r2+c2.
+// So you can write either of the following base cases as they all mean the same thing.
+
+// (r1==n-1 && c1==n-1)
+// (r2==n-1 && c2==n-1)
+// (r1==n-1 && c1==n-1) && (r2==n-1 && c2==n-1)
+
+     int dp[51][51][51];
+    int func(int r1 , int c1 , int r2 , int n , vector<vector<int>> &grid )
+    {
+        int c2=r1+c1-r2;
+        
+        if(r1>=n || r2>=n || c1>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1)
+        {
+            return INT_MIN;
+        }
+        
+        if(dp[r1][c1][r2]!=-1)
+        {
+            return dp[r1][c1][r2];
+        }
+        
+        if(r1==n-1 && c1==n-1)
+        {
+            return grid[r1][c1];
+        }
+        
+        int ans=grid[r1][c1];
+        
+        if(r1!=r2)
+        {
+            ans += grid[r2][c2];
+        }
+        
+        int temp= max(func(r1+1 , c1 , r2+1 , n , grid) , func(r1 , c1+1 , r2 , n , grid));
+        temp = max(temp , func(r1+1 , c1 , r2 , n, grid));
+        temp = max(temp , func(r1 , c1+1 , r2+1 , n , grid));
+        
+        ans+=temp;
+        
+        return dp[r1][c1][r2]=ans;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        memset(dp , -1 , sizeof(dp));
+        int n=grid.size();
+        return max(0 , func(0,0,0,n , grid));
+    }
+};
