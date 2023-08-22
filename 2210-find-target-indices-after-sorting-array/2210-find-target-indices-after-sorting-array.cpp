b828b0f5d4f88f12621cbcf4b7ca231275c8604c
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-22 20:38:39 +0530

    Time: 2 ms (87.89%), Space: 11.5 MB (96%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
@@ -1,74 +1,23 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        
-        sort(nums.begin(), nums.end());
 
-        int i=0,n=nums.size(),j=n-1,a=-1,b=-1;
+    int belowTCount = 0, tCount = 0, n=nums.size();
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] < target)
+            belowTCount++;
+        else if (nums[i] == target)
+            tCount++;
+    }
+    
+    vector<int> ans;
 
-        vector<int> ans;
-
-        while(i<=j){
-
-            int mid = (i+j)/2;
-
-            if(nums[mid] == target){
-
-                if(mid == 0 or nums[mid-1]!=target){
-
-                    a = mid;
-                    break;
-
-                }
-
-                else
-                j=mid-1;
-
-            }
-
-            else if(nums[mid]>target)
-            j=mid-1;
-
-            else
-            i=mid+1;
-
-        }
-
-        i=0, j=n-1;
-
-        while(i<=j){
-
-            int mid = (i+j)/2;
-
-            if(nums[mid] == target){
-
-                if(mid == n-1 or nums[mid+1]!=target){
-
-                    b = mid;
-                    break;
-
-                }
-
-                else
-                i=mid+1;
-
-            }
-
-            else if(nums[mid]>target)
-            j=mid-1;
-
-            else
-            i=mid+1;
-
-        }
-        
-        if(a==-1)
-        return ans;
-
-        for(int k=a;k<=b;k++)
-        ans.push_back(k);
-
-        return ans;
-
+    for (int t = 0; t < tCount; t++) {
+        ans.push_back(belowTCount++);
+    }
+    
+    return ans;
+    
     }
 };
