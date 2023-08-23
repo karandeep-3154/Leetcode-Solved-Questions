81c9701ccbff58d2ad54668a3e2038e83bd86782
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-23 10:24:29 +0530

    Time: 0 ms (100%), Space: 6.2 MB (46.07%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-	string reorganizeString(string s) {
-		vector<int> chrCounts(26);
-		for (auto& chr: s) { if(++chrCounts[chr-97] > (s.size()+1)/2) return "";}
-		priority_queue<pair<int, char>> max_heap;
-		for (int i = 0; i < 26; i++) {
-			if (chrCounts[i]) {max_heap.push({chrCounts[i], 97+i});}
-		}
-		int i = 0;
-		pair<int, char> prev = max_heap.top(); max_heap.pop();
-		s[i++] = prev.second;
-		while (not max_heap.empty()) {
-			pair<int, char> curr = max_heap.top(); max_heap.pop();
-			s[i++] = curr.second;
-			if (--prev.first) max_heap.push(prev);
-			prev = curr;
-		}
-		return s;
-	}
+    string reorganizeString(string s) {
+        vector<int> charCounts(26, 0);
+        for (char c : s) {
+            charCounts[c - 'a']++;
+        }
+        int maxCount = 0, letter = 0;
+        for (int i = 0; i < charCounts.size(); i++) {
+            if (charCounts[i] > maxCount) {
+                maxCount = charCounts[i];
+                letter = i;
+            }
+        }
+        if (maxCount > (s.length() + 1) / 2) {
+            return "";
+        }
+        string ans = s;
+        int index = 0;
+
+        // Place the most frequent letter
+        while (charCounts[letter] != 0) {
+            ans[index] = char(letter + 'a');
+            index += 2;
+            charCounts[letter]--;
+        }
+
+        // Place rest of the letters in any order
+        for (int i = 0; i < charCounts.size(); i++) {
+            while (charCounts[i] > 0) {
+                if (index >= s.length()) {
+                    index = 1;
+                }
+                ans[index] = char(i + 'a');
+                index += 2;
+                charCounts[i]--;
+            }
+        }
+
+        return ans;
+    }
 };
