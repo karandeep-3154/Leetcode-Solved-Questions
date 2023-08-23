f630dea0d920fd0b3a4e690e3b2d23e6fa849dcc
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-23 10:22:37 +0530

    Time: 3 ms (51.26%), Space: 6.2 MB (46.07%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
-    string reorganizeString(string S) {
-        string res="";
-        unordered_map<char,int> mp;
-        priority_queue<pair<int,char>>pq;
-        
-        for(auto s: S)
-            mp[s]+=1;
-        
-        for(auto m: mp)
-            pq.push(make_pair(m.second,m.first));
-        
-        while(pq.size()>1){
-            auto top1= pq.top();
-            pq.pop();
-            auto top2 = pq.top();
-            pq.pop();
-            
-            res+=top1.second;
-            res+=top2.second;
-            
-            top1.first -=1;
-            top2.first -= 1;
-            
-            if(top1.first > 0)
-                pq.push(top1);
-            
-            if(top2.first > 0)
-                pq.push(top2);
-        }
-        
-        if(!pq.empty()){
-            if(pq.top().first > 1)
-                return "";
-            
-            else
-                res+=pq.top().second;
-        }
-        
-        return res;
-    }
+	string reorganizeString(string s) {
+		vector<int> chrCounts(26);
+		for (auto& chr: s) { if(++chrCounts[chr-97] > (s.size()+1)/2) return "";}
+		priority_queue<pair<int, char>> max_heap;
+		for (int i = 0; i < 26; i++) {
+			if (chrCounts[i]) {max_heap.push({chrCounts[i], 97+i});}
+		}
+		int i = 0;
+		pair<int, char> prev = max_heap.top(); max_heap.pop();
+		s[i++] = prev.second;
+		while (not max_heap.empty()) {
+			pair<int, char> curr = max_heap.top(); max_heap.pop();
+			s[i++] = curr.second;
+			if (--prev.first) max_heap.push(prev);
+			prev = curr;
+		}
+		return s;
+	}
 };
