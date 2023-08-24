c7bca8efff0a55a6437dac1e4029b1f60cdb3b7c
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-24 10:48:44 +0530

    Time: 52 ms (94.23%), Space: 10.5 MB (94.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         
-        int dp[1000001];
+        int dp[13][10001];
 
-        memset(dp, 1000000, sizeof(dp));
+        dp[0][0] = 1;
+        for(int j=1;j<=amount;j++)
+        dp[0][j] = 100000;
 
-        dp[0] = 0;
+        for(int i=1;i<=coins.size();i++){
+            for(int j=0;j<=amount;j++){
 
-        for(auto i : coins)
-        if(i<1000001)
-        dp[i] = 1;
+                if(j==0)
+                dp[i][j] = 0;
 
-        for(int i=1;i<=amount;i++){
+                else{
 
-            for(auto j : coins){
+                    dp[i][j] = dp[i-1][j];
 
-                if(i-j>=0)
-                dp[i] = min(dp[i], 1+dp[i-j]);
+                    if(j-coins[i-1] >= 0)
+                    dp[i][j] = min(dp[i][j], 1+dp[i][j-coins[i-1]]);
+
+                }
 
             }
-
         }
 
-        return dp[amount]>1000000?-1:dp[amount];
+        return dp[coins.size()][amount] == 100000 ? -1 : dp[coins.size()][amount];
 
     }
 };
