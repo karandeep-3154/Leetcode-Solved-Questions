65bc2e19dcd3e851f8f9ac2d3517ec26e892395e
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-26 21:19:44 +0530

    Time: 68 ms (22.09%), Space: 15.9 MB (44.01%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,50 +1,77 @@
 class Solution {
 public:
 
-    int findmax(vector<int> &prefix, int val){
+     int largestRectangleArea(vector<int>& heights) {
+        
+        int n = heights.size(),j=0;
 
-        int len=0, curlen=0;
+        stack<int> s;
+        vector<int> right(n);
+        
+        for(int i=n-1;i>=0;i--){
+            
+            while(s.size() and heights[s.top()]>=heights[i])
+            s.pop();
+            
+            if(s.size())
+            right[i] = s.top()-1;
 
-        for(auto i : prefix){
-
-            if(i == val)
-            curlen++;
-
-            else{
-
-                len = max(len, curlen);
-                curlen = 0;
-
-            }
-
+            else
+            right[i]=n-1;
+            
+            s.push(i);
+            
         }
 
-        len = max(len, curlen);
-        return len;
+        vector<int> left(n);
+        stack<int> ss;s=ss;
+        
+        for(int i=0;i<n;i++){
+            
+            while(s.size() and heights[s.top()]>=heights[i])
+            s.pop();
+            
+            if(s.size())
+            left[i] = s.top()+1;
+
+            else
+            left[i]=0;
+            
+            s.push(i);
+            
+        }
+        
+        int ans=0;
+               
+        for(int i=0;i<n;i++)
+        ans=max(ans, heights[i]*(right[i]-left[i]+1));
+
+        return ans;
 
     }
 
     int maximalRectangle(vector<vector<char>>& matrix) {
         
-        int ans=0, m=matrix.size(), n=matrix[0].size();
+        int m=matrix.size(), n=matrix[0].size(), ans=0;
 
-        for(int i=0;i<n;i++){
-            
-            vector<int> prefix(m, 0);
+        vector<int> prefix(n, 0);
 
-            for(int j=i;j<n;j++){
+        for(int i=0;i<m;i++){
 
-                for(int k=0;k<m;k++)
-                prefix[k]+=matrix[k][j]-(int)'0';
+            for(int j=0;j<n;j++){
 
-                int height = findmax(prefix, j-i+1);
+                int val=matrix[i][j]-(int)'0';
 
-                if(height == 0)
-                break;
+                if(prefix[j]>=1 and val==0)
+                prefix[j]=0;
 
-                ans = max(ans, height*(j-i+1));
+                else
+                prefix[j]+=val;
 
             }
+
+            ans = max(ans, largestRectangleArea(prefix));
+
         }
 
         return ans;
