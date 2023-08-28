5ac58d32516e8ce4c90889884e6caa52c56cc4b9
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-28 14:29:54 +0530

    Time: 17 ms (88.27%), Space: 22.2 MB (80.42%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -19,28 +19,17 @@ public:
 
             int mid = (l+h)/2;
 
+            if(mid % 2 == 1)
+            mid--;
+
             if(nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1])
             return nums[mid];
 
-            else if(mid % 2 == 0){//even index
+            if(nums[mid+1] == nums[mid])
+            l=mid+1;
 
-                if(nums[mid+1] == nums[mid])
-                l=mid+1;
-
-                else
-                h=mid-1; 
-
-            }
-
-            else{
-
-                if(nums[mid-1] == nums[mid])
-                l=mid+1;
-
-                else
-                h=mid-1;
-
-            }
+            else
+            h=mid-1; 
 
         }
 
