f38e285e2b52fd31fb3d043ab369074fa38d9dd2
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-30 19:18:22 +0530

    Time: 119 ms (83.81%), Space: 57 MB (88.73%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,37 +1,55 @@
 class Solution {
 public:
+
     bool helper(vector<int>&nums, int k, int mid) {
+        
         int i = 0, n = nums.size();
+        
         while (i < n) {
+            
             if (nums[i] <= mid) {
                 k--;
                 i+=2;
             }
+            
             else {
                 i++;
             }
+            
             if (k == 0) {
                 return true;
             }
         }
+        
         return k == 0;
     }
 
     int minCapability(vector<int>& nums, int k) {
-        int n = nums.size();
+        
+        int n = nums.size();//it will be of pattern FFFFFFFFFTTTTTTTTTTT as there will be a certain no. x (ans) from which k no.s are lesser present in array then if a number y is choosen lessser than x is choosen there may not be k lesser no.s but if z is choosen greater than x choosen then definetly there would be k no.s lesser than z
         //traverse nums, check min and max
+        
         int low = 1, high = INT_MAX;
+        
         int ans = INT_MAX;
+        
         while (low <= high) {
+            
             int mid = low + (high-low)/2;
+            
             if (helper(nums, k, mid)) {
+                
                 ans =mid;
                 high = mid-1;
+            
             }
             else {
+                
                 low = mid+1;
+            
             }
         }
+        
         return ans;
     }
 };
