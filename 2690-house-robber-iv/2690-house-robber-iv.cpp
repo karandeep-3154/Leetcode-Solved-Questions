b1d5fe0d810615e77d6295a6cad321cd3430cffb
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2023-08-30 18:11:09 +0530

    Time: 127 ms (68.49%), Space: 57 MB (88.73%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool helper(vector<int>&nums, int k, int mid) {
+        int i = 0, n = nums.size();
+        while (i < n) {
+            if (nums[i] <= mid) {
+                k--;
+                i+=2;
+            }
+            else {
+                i++;
+            }
+            if (k == 0) {
+                return true;
+            }
+        }
+        return k == 0;
+    }
+
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+        //traverse nums, check min and max
+        int low = 1, high = INT_MAX;
+        int ans = INT_MAX;
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (helper(nums, k, mid)) {
+                ans = min(ans, mid);
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
