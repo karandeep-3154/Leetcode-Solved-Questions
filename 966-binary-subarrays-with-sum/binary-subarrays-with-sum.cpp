b7516d920c875dc342f22515436d64383f5c624f
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-03-14 21:46:18 +0530

    Time: 23 ms (74.99%) | Memory: 31.2 MB (66.62%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
+
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         
-        map<int, int> mp;
-        mp[0] = 1;
-
-        int s=0, ans=0;
-
-        for(auto i : nums){
-
-            s+=i;
-
-            ans += mp[s-goal];
-
-            mp[s]++;
-
+        int start = 0;
+        int prefixZeros = 0;
+        int currentSum = 0;
+        int totalCount = 0;
+        
+        // Initialize end pointer
+        int end = 0;
+        
+        // Loop through the array using while loop
+        while (end < nums.size()) {
+            // Add current element to the sum
+            currentSum += nums[end];
+            
+            // Slide the window while condition is met
+            while (start < end && (nums[start] == 0 || currentSum > goal)) {
+                if (nums[start] == 1) {
+                    prefixZeros = 0;
+                } else {
+                    prefixZeros += 1;
+                }
+                
+                currentSum -= nums[start];
+                start += 1;
+            }
+            
+            // Count subarrays when window sum matches the goal
+            if (currentSum == goal) {
+                totalCount += 1 + prefixZeros;
+            }
+            
+            // Move end pointer to the next element
+            end++;
         }
-
-        return ans;
-
+        
+        return totalCount;
     }
 };
