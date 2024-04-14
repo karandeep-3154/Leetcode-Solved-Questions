4838c59061073af5be0072bd59aff17f69f7fae1
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-04-14 17:07:05 +0530

    Time: 4 ms (9.68%) | Memory: 8.1 MB (12.76%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
+
+// REFER https://youtu.be/Pno2hATcwHA?si=1nY-uYZM1pyC-WbR 
+
+// https://www.youtube.com/watch?v=Ah8xgpoGIUE&t=1834s&pp=ygUYdmFsaWQgcGFyZW50aGVzaXMgc3RyaW5n
+    
     bool checkValidString(string s) {
-
-        int n = s.size();
         
-        stack<int> opening, asterisk;
+        int dp[101][102], n = s.size();//dp[i][j] denotes string of from index i till index n-1 with current opened brackets as j
+        memset(dp, 0, sizeof(dp));
+        dp[n][0] = 1;
 
-        for(int i=0;i<n;i++){
+        // for(int i=1;i<=n;i++)
+        // dp[i][0] = 1;//if no. of open brackets are 0 for any size of string s
+//above thing cannot always hold true as if string is )) and initially open brackets are 0, so we would say it is true but no, )) is still invalid. So, we are checking its validity as well 
 
-            if(s[i] == '(')
-            opening.push(i);
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=n;j++){
 
-            else if(s[i] == ')'){
+                if(s[i] == '(')
+                dp[i][j] = dp[i+1][j+1];
 
-                if(opening.size())
-                opening.pop();
+                else if(s[i] == ')'){
 
-                else if(asterisk.size())
-                asterisk.pop();
+                    if(j>0)
+                    dp[i][j] = dp[i+1][j-1];
 
-                else
-                return false;
+                }
 
+                else{
+
+                    if(j>0)
+                    dp[i][j] = dp[i+1][j-1] or dp[i+1][j] or dp[i+1][j+1];
+
+                    else
+                    dp[i][j] = dp[i+1][j] or dp[i+1][j+1];
+
+                }
             }
-
-            else if(s[i] == '*')
-            asterisk.push(i);
-
         }
 
-        if(asterisk.size()<opening.size())
-        return false;
-
-        while(opening.size()){
-
-            if(opening.top()>asterisk.top())
-            return false;
-
-            opening.pop();
-            asterisk.pop();
-
-        }
-
-        return true;
+        return dp[0][0];//as we want ans for string of size n from 0 to n-1 when initially cnt of opened brackets are 0
 
     }
 };
