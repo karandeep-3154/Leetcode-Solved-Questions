d3f59619dc4bf9ecf1efee8fb3ccc4e4ebf95e3f
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-04-22 14:32:46 +0530

    Time: 218 ms (55.56%) | Memory: 114.6 MB (11.11%) - LeetSync

--- 3404-minimum-number-of-operations-to-satisfy-conditions/minimum-number-of-operations-to-satisfy-conditions.cpp
+++ 3404-minimum-number-of-operations-to-satisfy-conditions/minimum-number-of-operations-to-satisfy-conditions.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+/*
+
+Approach:- Dp[i][j] denotes min. operations required to make all elements of ith column
+equal to j fulfilling the condition that (i-1)th col must not have elements of value j
+
+cnt[i][j] denotes the count of element j in ith column of grid
+
+If i am placing j at ith col, i am calculation dp[i][j] by 
+
+1) Operations required to make rest of the elements equal to j which are already not 
+(in ith col). so that will be no.of rows - freq of j in ith col => m - cnt[i][j]
+
+2)Operations req to make (i-1)th col equal to any no. not equal to j so that no. will be k => dp[i-1][k]
+
+*/
     int minimumOperations(vector<vector<int>>& grid) {
         
         int dp[1000][10], cnt[1000][10];
@@ -22,7 +37,7 @@ public:
 
         if(n == 1)
         return tempans;
-        
+
         for(int i=1;i<n;i++){
             for(int j=0;j<=9;j++){
                 dp[i][j] = 1e9;
