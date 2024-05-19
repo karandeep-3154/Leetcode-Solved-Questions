51108a27f65453204acb06720694033dd36716fb
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-05-19 15:35:45 +0530

    Time: 155 ms (100.00%) | Memory: 125.9 MB (100.00%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -2,9 +2,16 @@ class Solution {
 public:
 
 vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-    vector<int> ps{0};
-    for (int i = 1; i < nums.size(); ++i)
-        ps.push_back(ps.back() + (nums[i - 1] % 2 == nums[i] % 2));
+    vector<int> ps(nums.size(), -1);
+    ps[0] = 0;
+
+    for (int i = 1; i < nums.size(); ++i){
+        if(nums[i - 1] % 2 != nums[i] % 2)
+        ps[i] = ps[i-1];
+
+        else
+        ps[i] = i;
+    }
     vector<bool> res;
     for (const auto &q : queries)
         res.push_back(ps[q[0]] == ps[q[1]]);
