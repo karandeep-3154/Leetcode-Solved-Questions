0f03d6134ca466b10de09b1b8d53882562db08f3
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-05-23 21:04:40 +0530

    Time: 754 ms (21.08%) | Memory: 39.5 MB (26.65%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    void dfs(vector<int> &cur, int index, vector<int>& nums, int n, int k, int &ans) {
-        if (index == n) {
-            if (!cur.empty()) {
-                bool isBeautiful = true;
-                for (int i = 0; i < cur.size(); i++) {
-                    for (int j = i + 1; j < cur.size(); j++) {
-                        if (abs(cur[j] - cur[i]) == k) {
-                            isBeautiful = false;
-                            break;
-                        }
-                    }
-                    if (!isBeautiful) break;
-                }
-                if (isBeautiful) ans++;
-            }
+//Optmized Approach using Maps O(2^n*logn)
+    void dfs(map<int, int> &mp, int index, vector<int>& nums, int n, int k, int &ans) {
+        if (index == n){
+            ans++;
             return;
         }
 
-        // Include nums[index] in the current subset
-        cur.push_back(nums[index]);
-        dfs(cur, index + 1, nums, n, k, ans);
-        cur.pop_back();
+        if(!mp[nums[index]-k]){
 
-        // Do not include nums[index] in the current subset
-        dfs(cur, index + 1, nums, n, k, ans);
+            mp[nums[index]]++;
+            dfs(mp, index + 1, nums, n, k, ans);
+            mp[nums[index]]--;
+
+        }
+
+        dfs(mp, index + 1, nums, n, k, ans);
     }
 
     int beautifulSubsets(vector<int>& nums, int k) {
+
+        sort(nums.begin(), nums.end());
+
         int n = nums.size(), ans = 0;
-        vector<int> cur = {};
-        dfs(cur, 0, nums, n, k, ans);
-        return ans;
+        map<int, int> mp;
+        dfs(mp, 0, nums, n, k, ans);
+        return ans-1;
+    
     }
+
 };
