b7171fa8246db50a8ffce87431e81701cf7fa31a
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-06-08 23:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (75.00%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-    string clearDigits(string s) {
-        
-        int cnt = 0;
-        string ans = "";
-        
-        for(int i=s.size()-1;i>=0;i--){
-
-            if(isdigit(s[i]))
-            cnt++;
-
-            else{
-
-                if(cnt)
-                cnt--;
-
-                else
-                ans.push_back(s[i]);
-
-            }
-
-        }
-
-        reverse(ans.begin(), ans.end());
-
-        return ans;
-        
-    }
+  string clearDigits(string s) {
+    string res;
+    for (char ch : s)
+        if (isdigit(ch))
+            res.pop_back();
+        else
+            res += ch;
+    return res;
+}
 };
