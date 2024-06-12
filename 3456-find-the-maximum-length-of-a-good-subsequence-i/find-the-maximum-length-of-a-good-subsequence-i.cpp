936e4e56d501ed0f07d3381d99978bfa12e1f445
karandeep-3154 <96348619+karandeep-3154@users.noreply.github.com>
2024-06-12 16:02:23 +0530

    Time: 172 ms (89.15%) | Memory: 22.1 MB (95.76%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
 
-    int dp[500][26];
-
-    int solve(vector<int>& nums, int i, int k, int &ans){
-
-        if(i == 0)
-        return 1;
-
-        if(dp[i][k] != -1)
-        return dp[i][k];
-
-        solve(nums, i-1, k, ans);
-
-        int curans = 0;
-
-        for(int j=i-1;j>=0;j--){
-
-            if(nums[j] == nums[i])
-            curans = max(curans, solve(nums, j, k, ans));
-
-        }
-
-        if(k > 0){
-
-            for(int j=i-1;j>=0;j--){
-
-                if(nums[j] != nums[i])
-                curans = max(curans, solve(nums, j, k-1, ans));
-
-            }
-
-        }
-
-        ans = max(ans, curans+1);
-
-        return dp[i][k] = curans+1;
-
-    }
 
     int maximumLength(vector<int>& nums, int k) {
         
-        int ans = 1;
+        int dp[500][26], ans=1;
 
-        memset(dp, -1, sizeof(dp));
+        memset(dp, 0, sizeof(dp));
 
-        solve(nums, nums.size()-1, k, ans);
+        int n = nums.size();
+
+        for(int i=0;i<=25;i++)
+        dp[0][i] = 1;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=k;j++){
+
+                int curans = 0;
+
+                for(int p=i-1;p>=0;p--){
+
+                    if(nums[p] == nums[i])
+                    curans = max(curans, dp[p][j]);
+
+                }
+
+                if(j > 0){
+
+                    for(int p=i-1;p>=0;p--){
+
+                        if(nums[p] != nums[i])
+                        curans = max(curans, dp[p][j-1]);
+
+                    }
+
+                }
+
+                ans = max(ans, curans+1);
+
+                dp[i][j] = curans+1;
+
+            }
+        }
 
         return ans;
 
